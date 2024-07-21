6e70b0a442666b2c860926f4299c27648354a2cd
SCatella <samuel.catella@gmail.com>
2024-07-21 14:39:04 -0600

    setting logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+
+import './Logo.css';
 
 const Logo = () => {
   return (
-    <div>
+    <div className='ma4 mt0'>
+      <Tilt
+        className='Tilt'
+        style=
+        {{
+          height: '150px',
+          width: '150px',
+          backgroundColor: 'darkgreen',
+          tiltMaxAngleX: '55',
+          tiltMaxAngleY: '55',
+          transitionSpeed: '800'
+        }}
+      >
+        <img className='Tilt-inner pa3' src={brain} alt="brain logo" />
+      </Tilt>
     </div>
   )
 }
