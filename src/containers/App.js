b49f33cef5955dbc0cb4d7691a2ba6cb5af33a96
SCatella <samuel.catella@gmail.com>
2024-07-21 13:37:07 -0600

    imporoved folder structure, converted App.js to a class, added state, and added componentDidMount or use later. Basic setup is complete.

---
+++ src/containers/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <main>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
